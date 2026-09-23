72b670b51869a8bcabdb8a62d56ea7c0d63e0ef9
Laman Huseynova <Huseynova_Laman@student.ceu.edu>
2026-09-23 17:29:19 +0200

    comments

--- assignment.py
+++ assignment.py
@@ -1,6 +1,6 @@
-drink = "tea"
-sales = {"tea": 3, "coffee": 5}
-sold = sales[drink]
-sold = sold + 1
-print(drink)
-print(sold)
+drink = "tea" # drink is the variable and 'tea' is assigned to it. the output is going to be 'tea'
+sales = {"tea": 3, "coffee": 5} # sales is a list. the output will be 'a' and 'e'
+sold = sales[drink] # 
+sold = sold + 1 # whn printed it will say sold1
+print(drink) # asking to print the value of drink, which is tea
+print(sold) #asking to print the value od sold, which is sold1
